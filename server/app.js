1511a06d1656553655a557c0876f300bb4416f37
UtsavUpadhyay08 <utsavup2004@gmail.com>
2024-07-13 11:31:19 +0530

    Routes created

--- server/app.js
+++ server/app.js
@@ -1,9 +1,18 @@
 const express = require("express");
 const pool = require("./db");
-
 const app = express();
-const PORT = process.env.PORT || 3000;
 
+app.use(express.json());
+
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+
+app.use("/api/auth/register",);
+app.use("/api/auth/login",);
+app.use("/api/auth/logout",);
+// app.use("/api/users",);
+// app.use("/api/seeds",);
+// app.use("/api/events",);
+// app.use("/api/files",);
