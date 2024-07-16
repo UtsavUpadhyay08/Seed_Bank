5da4850715a4a70adc51d43fb03a40624df6872b
UtsavUpadhyay08 <utsavup2004@gmail.com>
2024-07-16 09:29:52 +0530

    seedRouter

--- server/routers/seedRouter.js
+++ server/routers/seedRouter.js
@@ -1,4 +1,25 @@
 const express = require("express");
 const seedRouter = express.Router();
 
+seedRouter.route("/")
+    .get()          //get all seeds
+    .post();        //if admin then can create new variety
+
+seedRouter.route("/:id")
+    .get()         //get particular type of seed
+//     .put()         //updates seed if transaction performed
+//     .delete();     //delete seed if quantity becomes empty
+
+// seedRouter.route("/deposit")
+//     .post();       //deposit seed
+
+// seedRouter.route("/retrieve")
+//     .post();       //retrieve seed
+
+// seedRouter.route("totaldeposited")
+//     .get();
+
+// seedRouter.route("totalretrieved")
+    // .get();
+
 module.exports = seedRouter;
