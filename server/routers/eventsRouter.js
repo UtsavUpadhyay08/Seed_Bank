054206b914b48402479c475b4f20530a4cdb6562
UtsavUpadhyay08 <utsavup2004@gmail.com>
2024-07-26 21:34:22 +0530

    Updated eventsRouter.js

--- server/routers/eventsRouter.js
+++ server/routers/eventsRouter.js
@@ -1,15 +1,22 @@
 const express = require("express");
 const { getAllEvents, createEvent, getEventById, putEvent, deleteEvent } = require("../controllers/eventController");
-const { protectRoute } = require("../controllers/authController");
+const { protectRoute, isAuthorised } = require("../controllers/authController");
 const eventsRouter = express.Router();
 
 eventsRouter.route("/")
-    .get(getAllEvents)          //get all events
-    .post(protectRoute,createEvent);        //if admin then can create new variety
+    .get(getAllEvents);          //get all events
 
 eventsRouter.route("/:id")
-    .get(getEventById)         //get particular type of event
-    .put(protectRoute,putEvent)         //updates event 
-    .delete(protectRoute,deleteEvent);     //deletes event
+    .get(getEventById);        //get particular type of event
+    
+eventsRouter.use(protectRoute);
+eventsRouter.use(isAuthorised("admin"));
+
+eventsRouter.route("/") 
+    .post(createEvent);        //if admin then can create new variety
+
+eventsRouter.route("/:id")
+    .put(putEvent)         //updates event 
+    .delete(deleteEvent);     //deletes event
 
 module.exports = eventsRouter;
