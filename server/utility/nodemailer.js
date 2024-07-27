60ff6d5f3d17c12cf241aeeba51cb3f6a1c88ff5
UtsavUpadhyay08 <utsavup2004@gmail.com>
2024-07-27 12:29:57 +0530

    Reset Password

--- server/utility/nodemailer.js
+++ server/utility/nodemailer.js
@@ -9,9 +9,24 @@ module.exports.sendMail = async function (str, data) {
             pass: process.env.PASS
         },
     });
-    // var sub, content;
-    // if (str == "signup") {
-    //     sub = "Verify Password";
-        
-    // }
+    var sub, content;
+    if (str == "signup") {
+        sub = "Verify Password";
+        content = `<h3>Verify your password by clicking below<br>${data.link}</h3>`;
+    }
+    else {
+        sub = "Reset Password";
+        content = `<h3>Reset your password by clicking below<br>${data.link}</h3>`;
+    }
+    try {
+        const info = await transport.sendMail({
+            from: process.env.USER,
+            to: data.email,
+            subject: sub,
+            html: content
+        });
+        console.log("Message Sent with ID:", info.messageId);
+    } catch (err) {
+        console.log(err.message);
+    }
 }
